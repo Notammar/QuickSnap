8cbb4fb2f508b16ea1eb8b4ba8892cdac29b00ba
Ammar Shaukat <100086848@students.swinburne.edu.my>
2020-09-14 22:12:09 +0800

    change sound

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -32,6 +32,8 @@ namespace CardGames
 				if (SwinGame.KeyTyped(KeyCode.vk_LSHIFT) && SwinGame.KeyTyped(KeyCode.vk_RSHIFT))
 				{
 					//TODO: add sound effects
+					SwinGame.LoadSoundEffectNamed("Slap", "slap.wav");
+					
 				}
 				else if (SwinGame.KeyTyped(KeyCode.vk_LSHIFT))
 				{
